a51c666486e8e86deae56fe6c83ccfc6e1a21fc6
Gerrit Wanderer <gerrit.wanderer@getkiwi.com>
2015-11-20 10:05:14 +0100

    support for system language

--- DeviceUtil.m
+++ DeviceUtil.m
@@ -9,17 +9,19 @@ RCT_EXPORT_MODULE();
 @synthesize bridge = _bridge;
 
 - (NSDictionary *)constantsToExport {
-    NSString *model = [[UIDevice currentDevice] model];
-    NSString *name = [[UIDevice currentDevice] name];
-    NSString *systemName = [[UIDevice currentDevice] systemName];
-    NSString *systemVersion = [[UIDevice currentDevice] systemVersion];
+  NSString *model = [[UIDevice currentDevice] model];
+  NSString *name = [[UIDevice currentDevice] name];
+  NSString *systemName = [[UIDevice currentDevice] systemName];
+  NSString *systemVersion = [[UIDevice currentDevice] systemVersion];
+  NSString *systenLanguage = [[NSLocale preferredLanguages] objectAtIndex:0];
 
-    return @{
-             @"model" : (model),
-             @"name" : (name),
-             @"systemName" : (systemName),
-             @"systemVersion" : (systemVersion)
-             };
+  return @{
+    @"model" : (model),
+    @"name" : (name),
+    @"systemName" : (systemName),
+    @"systemVersion" : (systemVersion),
+    @"systemLanguage" : (systemLanguage)
+  };
 }
 
 @end
